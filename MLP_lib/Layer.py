064af821300fd679ce9047136c8e185ca3a55a2a
Antoine Giordano <agiordan@z3r10p5.42lyon.fr>
2021-03-12 16:16:58 +0100

    MLP library structure begin

---
+++ MLP_lib/Layer.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+class Layer():
+
+	def __init__(self):
+		pass
+
+	def forward(self):
+		pass
+
+	def backward(self):
+	""" ?
+		Input: Loss
+		Output: dLoss / dwi
+	"""
+		pass
---
+++ MLP_lib/Model.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+class Model():
+
+	def __init__(self):
+		# self.sequence = []
+	
+	def add(self, obj):
+		# self.sequence.append(obj)
+
+	def compile(self):
+		pass
+	
+	def forward(self):
+		pass
+	
+	def fit(self):
+		pass
